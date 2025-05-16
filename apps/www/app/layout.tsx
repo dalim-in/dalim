3a1444ae863bcf35f903c819399fd7e07e89ee45
dalim-in <contact@dalim.in>
2025-05-17 00:05:13 +0530

    dalim

---
+++ apps/www/app/layout.tsx
@@ -0,0 +1,44 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from '@/components/theme-provider'  
+
+const geistSans = Geist({
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+    title: 'Shadcn Marketing Blocks | Tailark',
+    description: 'Speed up your workflow with responsive, pre-built UI blocks designed for marketing websites.',
+    keywords: 'shadcn, ui, blocks, marketing, components, tailwind, react, nextjs, tailark, responsive, design system, templates, template, hero section, features, footers, contact forms, Bento, Call To Action, Testimonials, logo cloud, integrations, content, team, pricing, login, sign in, logout, stats FAQ sections',
+}
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode
+}>) {
+    return (
+        <html
+            lang="en"
+            suppressHydrationWarning>
+            <body className={`${geistSans.variable} ${geistMono.variable} overflow-x-hidden antialiased`}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange>
+                    {children}
+                </ThemeProvider>
+                 
+            </body>
+            
+        </html>
+    )
+}
