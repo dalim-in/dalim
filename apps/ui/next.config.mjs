93aa5737c2507ded55ce7d9fe21b64a68a65cc8f
dalim-in <contact@dalim.in>
2025-05-21 11:50:09 +0530

    content

--- apps/ui/next.config.mjs
+++ apps/ui/next.config.mjs
@@ -1,25 +1,31 @@
+import { createContentlayerPlugin } from 'next-contentlayer2'
+
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true, 
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "ik.imagekit.io",
-      },
-    ],
-  },
+    reactStrictMode: true, 
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'res.cloudinary.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'ik.imagekit.io',
+            },
+        ],
+    },
 }
 
-export default nextConfig
+const withContentlayer = createContentlayerPlugin({
+    // Additional Contentlayer config options
+})
+
+export default withContentlayer(nextConfig)
