c7c425db906f0fc583c796e825b6c5def228e9bf
dalim-in <dalim.design@gmail.com>
2025-08-26 10:55:52 +0530

    ui test

--- apps/ui/next.config.mjs
+++ apps/ui/next.config.mjs
@@ -2,6 +2,7 @@ import { createMDX } from "fumadocs-mdx/next"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
